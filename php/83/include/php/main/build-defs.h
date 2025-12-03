7ee302ce1c0746074c99d34892510e4521671ed6
azdharsyahputra <if23.muhammadsyahputra@mhs.ubpkarawang.ac.id>
2025-12-03 11:01:15 +0700

    renew directory

---
+++ php/83/include/php/main/build-defs.h
@@ -0,0 +1,39 @@
+/*
+   +----------------------------------------------------------------------+
+   | Copyright (c) The PHP Group                                          |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://www.php.net/license/3_01.txt                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Stig Sæther Bakken <ssb@php.net>                             |
+   +----------------------------------------------------------------------+
+*/
+
+#define CONFIGURE_COMMAND " './configure'  '--prefix=/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php' '--disable-all' '--enable-fpm' '--with-fpm-user=azdhar' '--with-fpm-group=azdhar' '--enable-pdo' '--with-pdo-mysql' '--with-mysqli' '--enable-session' '--enable-filter' '--with-openssl' '--with-curl' '--with-zlib' '--enable-mbstring' '--enable-gd' '--with-zip' '--enable-sockets' '--enable-pcntl' '--with-readline' '--with-sodium' '--enable-ctype' '--enable-fileinfo' '--enable-tokenizer' '--enable-bcmath' '--enable-xml' '--with-libxml' '--enable-dom' '--enable-simplexml' '--enable-xmlwriter'"
+#define PHP_ODBC_CFLAGS	""
+#define PHP_ODBC_LFLAGS		""
+#define PHP_ODBC_LIBS		""
+#define PHP_ODBC_TYPE		""
+#define PHP_OCI8_DIR			""
+#define PHP_OCI8_ORACLE_VERSION		""
+#define PHP_PROG_SENDMAIL	"/usr/sbin/sendmail"
+#define PEAR_INSTALLDIR         ""
+#define PHP_INCLUDE_PATH	".:"
+#define PHP_EXTENSION_DIR       "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/lib/php/extensions/no-debug-non-zts-20230831"
+#define PHP_PREFIX              "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php"
+#define PHP_BINDIR              "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/bin"
+#define PHP_SBINDIR             "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/sbin"
+#define PHP_MANDIR              "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/php/man"
+#define PHP_LIBDIR              "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/lib/php"
+#define PHP_DATADIR             "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/share/php"
+#define PHP_SYSCONFDIR          "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/etc"
+#define PHP_LOCALSTATEDIR       "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/var"
+#define PHP_CONFIG_FILE_PATH    "/home/azdhar/evergon-build/php-8.3.0/../result/evergon-php/lib"
+#define PHP_CONFIG_FILE_SCAN_DIR    ""
+#define PHP_SHLIB_SUFFIX        "so"
+#define PHP_SHLIB_EXT_PREFIX    ""
